7eb664894d286bbc643e17e5da25df1e89ad6693
puppy-lol <77181404+puppy-lol@users.noreply.github.com>
2021-04-27 14:37:56 -0500

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyB1Pssz6SSezFjZR3JLYinNakIxomWb9zU",
+    authDomain: "twitter-37c08.firebaseapp.com",
+    projectId: "twitter-37c08",
+    storageBucket: "twitter-37c08.appspot.com",
+    messagingSenderId: "246615551625",
+    appId: "1:246615551625:web:d3ea9db6ab0a8d96672058",
+    measurementId: "G-V8NJTMJH67"
+  };
+  firebase.initializeApp(firebaseConfig);
